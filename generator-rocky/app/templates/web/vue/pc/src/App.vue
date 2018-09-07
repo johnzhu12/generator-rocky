cfec21d78df85c4b4bff8819a2d99f31dc917901
johnzhu12 <1042532367@qq.com>
2018-09-07 21:00:37 +0800

    update

--- generator-rocky/app/templates/web/vue/pc/src/App.vue
+++ generator-rocky/app/templates/web/vue/pc/src/App.vue
@@ -1,36 +1,96 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
+    <div class="container">
+        <div class="header">
+          <img src="" alt="">
+          <ul class="menu">
+            <li>
+              首页
+            </li>
+            <li>
+              首页
+            </li>
+            <li>
+              首页
+            </li>
+            <li>
+              首页
+            </li>
+          </ul>
+       </div>
+       <div class="body">
+         <div class="test">
+
+         </div>
+       </div>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
-<style>
+
+<style lang='scss'>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  margin: 0 10%;
+}
+.header {
+  padding: 20px 50px;
+  // background: red;
+  vertical-align: middle;
+
+  img {
+    width: 80px;
+    height: 80px;
+    background-color: black;
+    display: inline-block;
+
+    vertical-align: middle;
+  }
+  .menu {
+    width: 70%;
+    display: inline-block;
+    vertical-align: middle;
+    height: 80px;
+    overflow: hidden;
+    white-space: nowrap; //处理块元素中的空白符和换行
+    li {
+      height: 100%;
+      list-style: none;
+      display: inline-block; //使li对象显示为一行
+      margin-left: 15px;
+      width: 80px;
+      border-bottom: solid 5px red;
+      text-align: center;
+      padding: 10px 0;
+    }
+  }
+}
+.body {
+  height: 800px;
+  background: blue;
+  position: relative;
+  .test {
+    width: 100px;
+    height: 100px;
+    background: black;
+    position: absolute;
+    right: 10%;
+    top: 30%;
+  }
 }
 </style>
